6c05c47f06ef8c0d5e17e6a3716b8a4b145b51f2
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2026-06-02 04:33:42 +0000

    con errores para cambios

--- logica/Comparable.java
+++
@@ -1,5 +0,0 @@
-package miPrincipal;
-
-public interface Comparable<T> {
-    int compareTo(T elemento);
-}
--- logica/Criterio.java
+++
@@ -1,5 +0,0 @@
-package miPrincipal;
-
-public abstract class Criterio<T> {
-    public abstract int comparar(T elemento1, T elemento2);
-}
